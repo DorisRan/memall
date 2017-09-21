7a7b9f2d2a65ad10119f722f7001dca364a98653
Leon Qin <qinling.net@gmail.com>
2017-09-21 22:57:03 +0800

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { WeUiModule } from 'ngx-weui';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    WeUiModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { }
